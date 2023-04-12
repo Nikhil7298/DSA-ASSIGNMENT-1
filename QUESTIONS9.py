7763d2abf9baf6f5c8d64bfcba8093da36f96e8e
Nikhil Kurimilla <124423814+Nikhil7298@users.noreply.github.com>
2023-04-12 10:28:41 +0530

    Delete QUESTIONS9.py

--- QUESTIONS9.py
+++
@@ -1,5 +0,0 @@
-stack = list(map(int, input("enter values of stack separated by space : ").split()))
-rev_stack = []
-for _ in range(len(stack)):
-  rev_stack.append(stack.pop())
-print("reversed stack is",*rev_stack)
